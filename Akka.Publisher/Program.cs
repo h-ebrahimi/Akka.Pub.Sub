d151021b64e9926a2f6d35ae0868333dd8ccf12e
ho.ebrahimi <ho.ebrahimi@MPC10002218>
2022-06-28 17:17:30 +0430

    Add project files.

---
+++ Akka.Publisher/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using Akka.Actor;
+using Akka.Cluster;
+using Akka.Cluster.Hosting;
+using Akka.Cluster.Tools.PublishSubscribe;
+using Akka.Hosting;
+using Akka.Remote.Hosting;
+using Microsoft.Extensions.Hosting;
+using Petabridge.Cmd.Cluster;
+using Petabridge.Cmd.Host;
+using Petabridge.Cmd.Remote;
+
+public partial class Program
+{
+    static string clusterSystem = "Pub.Sub.System";
+    static int port = 8110;
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("SeedApp");
+
+        var appBuilder = new HostBuilder();
+        appBuilder.ConfigureServices((context, service) =>
+        {
+            service.AddAkka(clusterSystem, options =>
+            {
+                options
+                    .WithRemoting("localhost", port)
+                    .WithClustering(new ClusterOptions
+                    {
+                        Roles = new string[] { "Publisher" },
+                        SeedNodes = new[] {
+                        Address.Parse($"akka.tcp://{clusterSystem}@localhost:{port}")
+                        }
+                    })
+                    .AddPetabridgeCmd(cmd =>
+                    {
+                        Console.WriteLine("   PetabridgeCmd Added");
+                        cmd.RegisterCommandPalette(new RemoteCommands());
+                        cmd.RegisterCommandPalette(ClusterCommands.Instance);
+                    })
+                    .StartActors((actorSystem, actorRegistery) =>
+                    {
+                        var pubsub = DistributedPubSub.Get(actorSystem);
+                        var actor = actorSystem.ActorOf<Publisher>("Publisher");
+                        actor.Tell("This Message Sent From Publisher");
+                    });
+            });
+
+        });
+
+
+        var app = appBuilder.Build();
+        app.Run();
+        
+        Console.ReadLine();
+    }
+}
---
+++ Akka.Publisher/Publisher.cs
@@ -0,0 +1,15 @@
+﻿
+using Akka.Actor;
+using Akka.Cluster.Tools.PublishSubscribe;
+
+public partial class Program
+{
+    public sealed class Publisher : ReceiveActor
+    {
+        public Publisher()
+        {
+            var mediator = DistributedPubSub.Get(Context.System).Mediator;
+            Receive<string>(input => mediator.Tell(new Publish("content", input.ToUpperInvariant())));
+        }
+    }
+}
